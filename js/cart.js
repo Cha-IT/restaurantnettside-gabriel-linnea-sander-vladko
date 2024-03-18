6b40e39114e919bca9ee83bdba981c825ee70ead
Sander <sanderrasmussen@outlook.com>
2024-03-18 16:13:08 +0100

    Handlekurv Visualises added items + validation

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,9 @@
 const ProduktBakgrunn = document.querySelector("#shoppingcartAlert");
 const antallProdukter = document.querySelector("#CartNumber");
-const HandlekurvEl = document.querySelector("#ShoppingCart");
+const HandlekurvBtn = document.querySelector("#ShoppingCart");
+const HandlekurvEl = document.querySelector("#ShoppingCart_Items")
 
-HandlekurvEl.addEventListener("click", VisHandlekurv);
+HandlekurvBtn.addEventListener("click", VisHandlekurv);
 
 let cartItems = JSON.parse(sessionStorage.getItem("cartItems")) || [];
 
@@ -22,6 +23,10 @@ if (
   antallProdukter.style.visibility = "visible";
 }
 
+window.addEventListener("load", () => {
+  oppdaterHandlekurv();
+})
+
 function LeggtilProdukt(p) {
   const produkt = p.target.closest(".Produkt-Container");
   const produktInfo = {
@@ -29,15 +34,37 @@ function LeggtilProdukt(p) {
     beskrivelse: produkt.querySelector(".Produkt_Informasjon").textContent,
     pris: produkt.querySelector(".Prislapp").textContent,
     bilde: produkt.querySelector("img").src,
+    antall: 1,
   };
-  cartItems.push(produktInfo);
+
+  let produktEksisterer = false;
+
+  for(let i = 0; i < cartItems.length; i++) {
+    if(cartItems[i].navn === produktInfo.navn) {
+      cartItems[i].antall++;
+      produktEksisterer = true;
+      break;
+    }
+  }
+
+  if(!produktEksisterer) {
+    cartItems.push(produktInfo);
+  }
+
   sessionStorage.setItem("cartItems", JSON.stringify(cartItems));
   oppdaterHandlekurv();
 }
 
 function oppdaterHandlekurv() {
-  antallProdukter.textContent = cartItems.length;
-  if (cartItems.length > 0) {
+  let ProduktTall = 0;
+
+  cartItems.forEach(produkt => {
+    ProduktTall += produkt.antall;
+  });
+
+  antallProdukter.textContent = ProduktTall;
+
+  if (ProduktTall > 0) {
     ProduktBakgrunn.style.visibility = "visible";
     antallProdukter.style.visibility = "visible";
   } else {
@@ -52,4 +79,55 @@ function nullstillHandlekurv() {
   oppdaterHandlekurv();
 }
 
-function VisHandlekurv() {}
+function VisHandlekurv() {
+  HandlekurvBtn.removeEventListener("click", VisHandlekurv);
+  HandlekurvBtn.addEventListener("click", LukkHandleKurv);
+
+  HandlekurvEl.innerHTML = ''
+  cartItems.forEach(produkt => {
+    new CartProdukt(produkt);
+  });
+  HandlekurvEl.style.visibility = "visible";
+
+  function LukkHandleKurv() {
+    HandlekurvEl.style.visibility = "hidden";
+    HandlekurvBtn.removeEventListener("click", LukkHandleKurv);
+    HandlekurvBtn.addEventListener("click", VisHandlekurv);
+  }
+}
+
+class CartProdukt {
+  constructor(produktInfo) {
+    this.produktnavn = produktInfo.navn;
+    this.bilde = produktInfo.bilde;
+    this.antallProdukt = produktInfo.antall;
+    this.opprettHandlekurvProdukt();
+    this.appendProdukter();
+  }
+
+  opprettHandlekurvProdukt() {
+    this.container = document.createElement("div");
+    this.container.classList.add("ShoppingCart_Item");
+
+    this.CartProduct = document.createElement("div");
+    this.CartProduct.classList.add("CartProduct-Container");
+    this.CartProduct.style.zIndex = "1";
+
+    this.ProduktBilde = document.createElement("img");
+    this.ProduktBilde.src = this.bilde;
+
+    this.TekstContainer = document.createElement("div");
+    this.Navn = document.createElement("p");
+    this.Navn.textContent = this.produktnavn;
+
+    this.Antall = document.createElement("p");
+    this.Antall.textContent = "Antall: " +  this.antallProdukt;
+  }
+
+  appendProdukter() {
+    this.TekstContainer.append(this.Navn, this.Antall)
+    this.CartProduct.append(this.ProduktBilde, this.TekstContainer)
+    this.container.appendChild(this.CartProduct)
+    HandlekurvEl.append(this.container)
+  }
+}
